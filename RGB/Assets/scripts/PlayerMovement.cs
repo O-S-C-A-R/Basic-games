3ebe311739d6560a40d8296bd0589338457bddc5
O-S-C-A-R <ostiong@s.sfusd.edu>
2017-07-03 14:38:54 -0700

    sure

---
+++ RGB/Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour 
+{
+	public float moveSpeed;
+	private Vector3 input;
+
+
+	void Start () 
+	{
+		
+	}
+
+	void Update ()
+	{
+		if(Get
+		input = new Vector3 (Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+	}
+}
